06813cb7c87a5d295b4e4256b8a616346c058d93
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-28 02:15:02 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,13 +2,17 @@ class Application
 
 
   @@items = []
-  
+
   def call(env)
 
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    #if req.path =="/items/"
+    if req.path.match(/items/)
+      resp.write "items!"
+
+    else
+      resp.status = 404
   end
 
 end
