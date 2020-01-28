ec45785c3dacb0f5621fd24a4b0db637583cb283
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-28 02:52:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path =="/items/"
-      item_name = req.path.split("/").last
+      item_name = req.path.split("/items/").last
       if item =@@items.find {|i| i.name == item_name}
         resp.write item.price
       else
