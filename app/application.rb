cb8c6619ab64191f51a12869d258bdefafe9f18e
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-28 02:15:49 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -8,7 +8,7 @@ class Application
     resp = Rack::Response.new
     req = Rack::Request.new(env)
 
-    if req.path.match(/items/)
+    if req.path =="/items/"
       resp.write "items!"
 
     else
