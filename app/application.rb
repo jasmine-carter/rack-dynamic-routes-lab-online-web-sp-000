b1560273e8c3b9de6ae13bc146206d7eb733589b
Jasmine Carter <jasmine.carter87@gmail.com>
2020-01-28 02:32:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,6 @@ class Application
         resp.write item.price
       else
         resp.write "Item not found"
-        resp.status = 404
       end
     else
       resp.write "Route not found"
